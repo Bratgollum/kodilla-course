ad9c87fbe7afe0e099993cae65573221d1b33f49
Bratgollum <bratgollum@gmail.com>
2019-01-08 19:15:57 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,43 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main (String[] args){
-        System.out.println("Moduł 6  wprowadzenie do testsowania" );
+
+     ////// Test Klasy Calculator
+
+     System.out.println("\nTest Klasy : Calculator\n");
+
+        Calculator calculator1 = new Calculator(40,24);
+
+        int calculator1NumberA = calculator1.getNumberA();
+        int calculator1NumberB = calculator1.getNumberB();
+        System.out.println("1.Sprawdzenie poprawnosci przypisania liczb ");
+
+        if(calculator1NumberA == 40 & calculator1NumberB == 24){
+            System.out.println("- przypisanie liczb dziala poprawnie ");
+        }
+        else {
+            System.out.println("- blad przy przypisaniu liczb");
+        }
+     /////// Test Dodawania A i B
+        System.out.println("\n2.Sprawdzenie metody dodawania: aAddB");
+        if((calculator1NumberA+calculator1NumberB)== calculator1.aAddB()){
+            System.out.println("- metoda dziala poprawnie a wynik z dodawania = "+calculator1.aAddB());
+        }
+        else{
+            System.out.println("- wystapil blad w metodzie ");
+        }
+        /////// Test odejmowania A i B
+        System.out.println("\n2.Sprawdzenie metody odejmowania: aSubtractB");
+        if((calculator1NumberA-calculator1NumberB)== calculator1.aSubtractB()){
+            System.out.println("- metoda dziala poprawnie a wynik z odejmowania = "+calculator1.aSubtractB());
+        }
+        else{
+            System.out.println("- wystapil blad w metodzie ");
+        }
+
 
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    private int numberA ;
+    private int numberB ;
+
+    public Calculator(int numberA , int numberB){
+        this.numberA = numberA;
+        this.numberB = numberB;
+    }
+
+    public int getNumberA(){
+        return numberA;
+    }
+    public int getNumberB(){
+        return numberB;
+    }
+    public int aAddB(){
+        return numberA + numberB;
+    }
+    public int aSubtractB(){
+        return numberA - numberB;
+    }
+
+}
