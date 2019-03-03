91a1d49c10ae2d5a9f3fb36183494c2fdd90ee31
Bratgollum <bratgollum@gmail.com>
2019-03-03 11:36:08 +0100

    Zadanie 13.1 do sprawdzenia

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,41 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.List;
+import java.util.*;
+
+class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+
+    public static void main( String[] args ){
+
+        MovieStore movieStore = new MovieStore();
+        Map<String, List<String>> listTitles = new HashMap<>();
+            listTitles.putAll(movieStore.getMovies());
+        for(Map.Entry<String,List<String>> list : listTitles.entrySet()){
+                list.getValue().stream().forEach(n -> {System.out.print(n+ "! ");});
+        }
+    }
+    }
+
