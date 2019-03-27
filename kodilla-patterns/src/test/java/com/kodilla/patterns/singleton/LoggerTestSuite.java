bf19cb9a3ce8b39250cb7f0df6f6be51d43db110
Bratgollum <bratgollum@gmail.com>
2019-03-27 19:48:11 +0100

    Unit 15.1 Singleton Logger

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,4 +1,22 @@
 package com.kodilla.patterns.singleton;
 
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class LoggerTestSuite {
+    @BeforeClass
+    public static void log(){
+        Logger.getInstance().log("My last log at 13:00");
+    }
+    @Test
+    public void testGetLastLog(){
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        System.out.println("Last log : " + lastLog);
+        //Then
+        Assert.assertEquals("My last log at 13:00", lastLog);
+    }
 }
